25cffaa88526a0269548198e7853e6cf609e6f8a
oji <yo-00331pc@outlook.com>
2025-05-25 23:12:57 +0900

    change return value from order_number to order_index

--- cafe.rb
+++ cafe.rb
@@ -20,8 +20,9 @@ def take_order(menus)
   end
   print '>'
   order_number = gets.to_i
-  puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
-  order_number
+  order_index = order_number - 1
+  puts "#{menus[order_index][:name]}(#{menus[order_index][:price]}円)ですね。"
+  order_index
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
